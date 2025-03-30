46c529443bb458076a3ee771299b5fcfc26cc261
Hamidou1089-1 <hamidoudiallo1844@gmail.com>
2025-03-30 22:42:02 +0200

    Network made abstract, because we will have several kind of network in the futur, stay tune

--- src/model/RandomNetwork.py
+++ src/model/RandomNetwork.py
@@ -12,8 +12,8 @@ class RandomNetwork(Network):
 
     Enfaite cette classe : nous permet de generer le reseau de depart.
 
-    Un choix que j'ai fais mais qui va etre embettant apres, je me débrouille pour que le net worth soit toujours positif, mais lorque je vais instancier, faut que j'interdise cette possibilité.
-    Car pour le moment c'est juste generer en random.
+    Un choix que j'ai fait, mais qui va être embettant apres, je me débrouille pour que le net worth soit toujours positif, mais lorque, je vais instancier, il faut que j'interdise cette possibilité.
+    Car pour le moment, c'est juste generer en random.
     """
 
     def __init__(self, number_bank: int, probability_of_linking=0.1):
@@ -23,19 +23,13 @@ class RandomNetwork(Network):
         :param at_random: Do you want to generate at random ?
         :param probability_of_linking: If you want to generate at random, what should the probability of linking ?
         """
-        self.number_bank = number_bank
+        super().__init__(number_bank)
         self.probability_of_linking = probability_of_linking
         self.generate()
-        self.due_payements = self.matrix_obligation @ np.array([1]*self.number_bank) + self.vector_outside_liabilities
-        self.matrix_relative_liabilities = np.zeros((self.number_bank, self.number_bank))
-        for k in range(number_bank):
-            for j in range(number_bank):
-                if self.due_payements[k] == 0:
-                    self.matrix_relative_liabilities[k][j] = 0
-                else:
-                    self.matrix_relative_liabilities[k][j] = self.matrix_obligation[k][j]/self.due_payements[k]
 
-        self.default_vector = [self.banks[i].is_default() for i in range(self.number_bank)]
+
+
+
 
 
 
@@ -63,7 +57,28 @@ class RandomNetwork(Network):
             else:
                 self.vector_outside_asset[i] = np.random.uniform(1, n**3)
 
+        self.due_payements = self.matrix_obligation @ np.array([1]*self.number_bank) + self.vector_outside_liabilities
+
+        self.vulnerabilities = (self.due_payements - self.vector_outside_liabilities)/self.vector_outside_liabilities
+
+        for k in range(n):
+            for j in range(n):
+                if self.due_payements[k] == 0:
+                    self.matrix_relative_liabilities[k][j] = 0
+                else:
+                    self.matrix_relative_liabilities[k][j] = self.matrix_obligation[k][j]/self.due_payements[k]
+
+        for i in range(n):
+            for j in range(n):
+                if self.matrix_relative_liabilities[j][i] == 0 or self.net_worth[i] == 0:
+                    self.relative_vulnerabilities[i][j] = 0
+                else:
+                    self.relative_vulnerabilities[i][j] = self.matrix_relative_liabilities[j][i]*(self.vector_outside_asset[j] - self.net_worth[j])/self.net_worth[i]
 
         for i in range(n):
             self.banks[i] = Bank(self.vector_outside_asset[i], on_me_doit[i], self.vector_outside_liabilities[i], je_dois[i])
             self.net_worth[i] = self.banks[i].balance
+
+        self.default_vector = [self.banks[i].is_default() for i in range(self.number_bank)]
+
+        return
--- src/model/network.py
+++ src/model/network.py
@@ -5,11 +5,35 @@ from abc import ABC, abstractmethod
 
 class Network(ABC):
     """
-    Interface abstraite pour different type de reseau:
+    Interface abstraite pour different type de réseau:
     - Random
     - Small World
     - Scale free
     """
+
+    def __init__(self, number_of_bank: int):
+
+        """
+        All these variable, are common to any kind of network that want to model a financial network.
+        :param number_of_bank:
+        """
+        self.number_bank = number_of_bank
+        """
+        beta_i = (due_payement_i - outside_liabilities_i)/due_payement_i
+        """
+        self.vulnerabilities = np.array([0]*self.number_bank)
+
+        self.relative_vulnerabilities = np.zeros((self.number_bank, self.number_bank))
+
+        self.matrix_obligation = np.zeros((self.number_bank, self.number_bank))
+        self.net_worth = np.array([0]*self.number_bank)
+        self.vector_outside_asset = np.array([0]*self.number_bank)
+        self.banks = np.empty(self.number_bank, dtype=object)
+        self.vector_outside_liabilities = np.array([0] * self.number_bank)
+        self.matrix_relative_liabilities = np.zeros((self.number_bank, self.number_bank))
+        self.due_payements = np.array([0]*self.number_bank)
+        self.default_vector = np.array([0]*self.number_bank)
+
 
     @abstractmethod
     def generate(self):
